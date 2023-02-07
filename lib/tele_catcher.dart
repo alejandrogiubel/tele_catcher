ca51fe83a047d5f93e086bef9594ba7de1500ab2
Alejo <agiubel@gmail.com>
2023-02-07 13:46:55 -0500

    feat: Add more app info to report

--- lib/tele_catcher.dart
+++ lib/tele_catcher.dart
@@ -1,6 +1,7 @@
 library tele_catcher;
 
 import 'dart:io';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 
@@ -23,11 +24,23 @@ class TeleCatcher {
     final platform = Platform.operatingSystem;
     final timeStamp = DateTime.now();
     final osVersion = Platform.operatingSystemVersion;
+
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    final appName = packageInfo.appName;
+    final packageName = packageInfo.packageName;
+    final version = packageInfo.version;
+    final buildNumber = packageInfo.buildNumber;
+
     final report = '''
 Hello, TeleCatcherBot reporting 🫡.
 OS: $platform
 OS Version: $osVersion
 Time stamp: $timeStamp
+App name: $appName
+Package name: $packageName
+App version: $version
+Build number: $buildNumber
 Report:
 $infoToReport
     ''';
